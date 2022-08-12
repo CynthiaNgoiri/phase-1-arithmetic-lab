d7276ee6ef020f7b6d0056f8596138e1ea020dab
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-08-12 15:26:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+  
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+let random=Math.floor(Math.random())+1;
+
+
+    let num3= 34;
+    let num4=5;
+
+const mod= num3 % num4;
+
+let max=Math.max(10,15,20);
+  
